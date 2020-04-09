c3e0eafb2f8477379b2b173d00429ec22db22ecc
Juarez Lopes <juarezlopesfjr@gmail.com>
2020-04-09 15:39:31 -0300

    Fixed running bugs

--- app/src/main/java/com/rubdev/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/rubdev/currencyconverter/MainActivity.kt
@@ -1,23 +1,16 @@
 package com.rubdev.currencyconverter
 
 import ViewHolder
-import android.content.DialogInterface
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.PointerIcon
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import com.rubdev.currencyconverter.R.id.button_calculate
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
-import kotlinx.android.synthetic.main.activity_main.view.textView
+import androidx.appcompat.app.AppCompatActivity
 import retrofit2.Call
 import retrofit2.Response
 import javax.security.auth.callback.Callback
-import kotlin.math.absoluteValue
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -34,11 +27,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         mViewHolder.textEuro = findViewById(R.id.text_valorEuro)
         //mViewHolder.buttonCalculate = findViewById(R.id.button_calculate)
 
-        var btnCalcular = findViewById<Button>(button_calculate)
+        val btnCalcular = findViewById<Button>(R.id.button_calculate)
         btnCalcular.setOnClickListener(this)
 
 
-        clearValues()
+//        clearValues()
     }
 
     override fun onClick(v: View) {
@@ -56,42 +49,42 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     fun getData() {
         val retrofitClient = NetworkUtils
-                                .getRetrofitInstance("https://economia.awesomeapi.com.br/json")
+            .getRetrofitInstance("https://economia.awesomeapi.com.br/json")
 
         val endpoint = retrofitClient.create(EndPoint::class.java)
         val callback = endpoint.getDolar()
 
-        callback.enqueue(object : Callback<List<Cotacoes>>, retrofit2.Callback<List<Cotacoes>> {
-            override fun onFailure(call: Call<List<Cotacoes>>, t: Throwable){
+        callback.enqueue(object : Callback, retrofit2.Callback<List<Cotacoes>> {
+            override fun onFailure(call: Call<List<Cotacoes>>, t: Throwable) {
                 Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
             }
 
-            override fun onResponse(call: Call<List<Cotacoes>>, response: Response<List<Cotacoes>>){
+            override fun onResponse(call: Call<List<Cotacoes>>, response: Response<List<Cotacoes>>) {
                 response.body()?.forEach {
-                  //  textView.text
+                    //  textView.text
 
-                }
                 }
             }
-        }
-
-    })
-
-
-    private fun clearValues() {
-        mViewHolder.textDolar!!.text = ""
-        mViewHolder.textEuro!!.text = ""
-    }
-
-
-    private class ViewHolder {
-        var editValue: EditText? = null
-        var textDolar: TextView? = null
-        var textEuro: TextView? = null
-        var buttonCalculate: Button? = null
+        })
     }
 
 }
+
+
+/*private fun clearValues() {
+    mViewHolder.textDolar!!.text = ""
+    mViewHolder.textEuro!!.text = ""
+}*/
+
+
+private class ViewHolder {
+    var editValue: EditText? = null
+    var textDolar: TextView? = null
+    var textEuro: TextView? = null
+    var buttonCalculate: Button? = null
+}
+
+
 
 
 
